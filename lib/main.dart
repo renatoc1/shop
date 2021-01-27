5ee6fddc472ac3590f5ed8ac946682aa24bde2ba
Renato Prata <renprata@hotmail.com>
2021-01-26 22:21:27 -0300

    Gerenciamento de Estado

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/providers/cart.dart';
+import 'package:shop/providers/products.dart';
+import 'package:shop/utils/app_routes.dart';
+import 'package:shop/views/cart_screen.dart';
+import 'package:shop/views/products_overview_screen.dart';
+import 'package:shop/views/produto_detail_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => Products(),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => Cart(),
+        ),
+      ],
+      child: MaterialApp(
+        title: 'Minha Loja',
+        theme: ThemeData(
+          primarySwatch: Colors.purple,
+          accentColor: Colors.deepOrange,
+          fontFamily: 'Lato',
+        ),
+        home: ProductOverviewScreen(),
+        routes: {
+          AppRoutes.PRODUCT_DETAIL: (ctx) => ProductDetailScreen(),
+          AppRoutes.CART: (ctx) => CartScreen(),
+        },
+      ),
+    );
+  }
+}
